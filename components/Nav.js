2e995c0224c5845865c936cf3e7dc1bae04f9559
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-04-09 12:42:22 -0400

    Partner assets add and global style button fixed

--- components/Nav.js
+++ components/Nav.js
@@ -13,7 +13,7 @@ export default function Nav (color = 'green') {
       <ul>
         <li>
           <a href="chisiamo.html">Chi siamo</a>
-          <a href="#">Diventa Rallista</a>
+          <a href="partner.html">Diventa Rallista</a>
           <a href="#">Diventa Partner</a>
         </li>
       </ul>
